dafc2ef00b846b18d583577ac4fb75d007115c1b
Shai1-Levi <73644506+Shai1-Levi@users.noreply.github.com>
2021-05-26 21:49:11 +0300

    trying to implement search player

--- project/routes/search.js
+++ project/routes/search.js
@@ -1,37 +1,42 @@
 let express = require("express");
 let router = express.Router();
 const players_utils = require("./utils/players_utils");
+let object_to_search = "";
+
+// var url = "http://www.example.com/index.php?myParam=384&login=admin"; // or window.location.href for current url
+// var captured = /myParam=([^&]+)/.exec(url)[1]; // Value is in [1] ('384' in our case)
+// var result = captured ? captured : 'myDefaultValue';
 
 
-router.use("/?q=", ()=>{
-    var url = window.location.search;
-    url = url.replace("?", ''); // remove the ?
-    alert(url); //alerts ProjectID=462 is your case
-});
 
-router.get("/players/:id", async(req, res, next) =>{
+router.get("/players/:searchQuery", async(req, res, next) =>{
     try{
-        const player_details = await players_utils.getPlayersInfo();
+        const player_details = await players_utils.searchPlayersInfoByName(req.params.searchQuery);
+        res.send(player_details);
     }
     catch(error){
         next(error);
     }
 });
 
-router.get("/teams/:id", async (req, res, next) =>{
-    try{
+// router.get("/teams/:searchQuery", async (req, res, next) =>{
+//     try{
 
-    }
-    catch(error){
-        next(error);
-    }
-});
+//     }
+//     catch(error){
+//         next(error);
+//     }
+// });
 
-router.get("/coaches/:id", async (req, res, next) =>{
-    try{
-    const coach_details = await players_utils.getCoachesInfo();
-    }
-    catch(error){
-        next(error);
-    }
-});
+// router.use("/coaches", async (req, res, next) =>{
+//     try{
+//     const coach_details = await players_utils.getCoachesInfo();
+//     res.send(coach_details);
+//     }
+//     catch(error){
+//         next(error);
+//     }
+// });
+
+
+module.exports = router;
